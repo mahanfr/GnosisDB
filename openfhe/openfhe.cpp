9a1e5cf69a606b2bdfc4502ae29150909fefc488
mahanfr <mahan.farzaneh@gmail.com>
2025-07-31 21:40:46 +0330

    separate functionality to functions

--- openfhe/openfhe.cpp
+++ openfhe/openfhe.cpp
@@ -5,10 +5,10 @@
 
 using namespace lbcrypto;
 
-int main() {
-    uint32_t plaintextModules = 2;
-    uint32_t numHops = 13;
+const uint32_t plaintextModules = 2;
+const uint32_t numHops = 13;
 
+CryptoContext<DCRTPoly> init() {
     CCParams<CryptoContextBGVRNS> params;
     params.SetPlaintextModulus(plaintextModules);
     params.SetScalingTechnique(FIXEDMANUAL);
@@ -26,10 +26,67 @@ int main() {
     cc->Enable(LEVELEDSHE);
     cc->Enable(PRE);
     // cryptoContext->Enable(ADVANCEDSHE);
+    return cc;
+}
 
-    ////////////////////////////////////////////////////////////
+KeyPair<DCRTPoly> genKeyPair(CryptoContext<DCRTPoly> cc) {
+
+    KeyPair<DCRTPoly> keyPair = cc->KeyGen();
+
+    if (!keyPair.good()) {
+        std::cout << "Key generation failed!" << std::endl;
+        exit(1);
+    }
+    return keyPair;
+}
+
+Plaintext encodePlainText(CryptoContext<DCRTPoly> cc, std::vector<uint8_t> data) {
+    std::vector<int64_t> vShorts;
+    for (size_t i = 0; i < data.size(); i++) {
+        char mchar = data[i];
+        vShorts.push_back(((mchar >> 7) & 1) % plaintextModules);
+        vShorts.push_back(((mchar >> 6) & 1) % plaintextModules);
+        vShorts.push_back(((mchar >> 5) & 1) % plaintextModules);
+        vShorts.push_back(((mchar >> 4) & 1) % plaintextModules);
+        vShorts.push_back(((mchar >> 3) & 1) % plaintextModules);
+        vShorts.push_back(((mchar >> 2) & 1) % plaintextModules);
+        vShorts.push_back(((mchar >> 1) & 1) % plaintextModules);
+        vShorts.push_back(((mchar >> 0) & 1) % plaintextModules);
+    }
+    return cc->MakeCoefPackedPlaintext(vShorts);
+}
+
+Ciphertext<DCRTPoly> encrypt(CryptoContext<DCRTPoly> cc, PublicKey<DCRTPoly> pubKey, Plaintext pt) {
+    return cc->Encrypt(pubKey, pt);
+}
+
+Plaintext decrypt(CryptoContext<DCRTPoly> cc,
+        PrivateKey<DCRTPoly> secKey,
+        Ciphertext<DCRTPoly> ct,
+        size_t ptSize) {
+    Plaintext pt;
+    cc->Decrypt(secKey, ct, &pt);
+    pt->SetLength(ptSize);
+    return pt;
+}
+
+EvalKey<DCRTPoly> genReKey(CryptoContext<DCRTPoly> cc, PrivateKey<DCRTPoly> oldSecKey, PublicKey<DCRTPoly> newPubKey) {
+    return cc->ReKeyGen(oldSecKey, newPubKey);
+}
+
+std::vector<int64_t> decodePlaintext(Plaintext pt, size_t ptSize) {
+    std::vector<int64_t> unpacked = pt->GetCoefPackedValue();
+    for(unsigned int j=0; j< ptSize; j++) {
+        if (unpacked[j] < 0)
+            unpacked[j] += plaintextModules;
+    }
+    return unpacked;
+}
+
+int main() {
+
+    auto cc = init();
     // PRINT PARAMS
-    ////////////////////////////////////////////////////////////
     std::cout << "p = " << cc->GetCryptoParameters()->GetPlaintextModulus() << std::endl;
     std::cout << "n = " << cc->GetCryptoParameters()->GetElementParams()->GetCyclotomicOrder() / 2
               << std::endl;
@@ -41,84 +98,34 @@ int main() {
     auto ringsize = cc->GetRingDimension();
     std::cout << "You can encrypt " << ringsize / 8 << "bytes of data" << std::endl;
 
-    ////////////////////////////////////////////////////////////
     // Perform Key Generation Operation
-    ////////////////////////////////////////////////////////////
-    KeyPair<DCRTPoly> keyPair1;
-    keyPair1 = cc->KeyGen();
+    KeyPair<DCRTPoly> keyPair1 = genKeyPair(cc);
 
-    if (!keyPair1.good()) {
-        std::cout << "Key generation failed!" << std::endl;
-        exit(1);
-    }
-
-    ////////////////////////////////////////////////////////////
     // Plaintext encoding
-    ////////////////////////////////////////////////////////////
     std::string message = "Secret Hello World\n";
-    std::vector<int64_t> vShorts;
-    for (size_t i = 0; i < message.length(); i++) {
-        char mchar = message[i];
-        vShorts.push_back(((mchar >> 7) & 1) % plaintextModules);
-        vShorts.push_back(((mchar >> 6) & 1) % plaintextModules);
-        vShorts.push_back(((mchar >> 5) & 1) % plaintextModules);
-        vShorts.push_back(((mchar >> 4) & 1) % plaintextModules);
-        vShorts.push_back(((mchar >> 3) & 1) % plaintextModules);
-        vShorts.push_back(((mchar >> 2) & 1) % plaintextModules);
-        vShorts.push_back(((mchar >> 1) & 1) % plaintextModules);
-        vShorts.push_back(((mchar >> 0) & 1) % plaintextModules);
-    }
-    Plaintext pt = cc->MakeCoefPackedPlaintext(vShorts);
+    std::vector<uint8_t> vec(message.begin(), message.end());
+    Plaintext pt = encodePlainText(cc, vec);
+    auto ptSize = pt->GetLength();
     
-    ////////////////////////////////////////////////////////////
     // Encryption
-    ////////////////////////////////////////////////////////////
-    auto ct1 = cc->Encrypt(keyPair1.publicKey, pt);
+    auto ct1 = encrypt(cc, keyPair1.publicKey, pt);
     
-
-    ////////////////////////////////////////////////////////////
     // Decryption of ciphertext
-    ////////////////////////////////////////////////////////////
-    Plaintext ptDec1;
-    cc->Decrypt(keyPair1.secretKey, ct1, &ptDec1);
-    ptDec1->SetLength(pt->GetLength());
-
+    Plaintext ptDec1 = decrypt(cc, keyPair1.secretKey, ct1, ptSize);
     
-    ////////////////////////////////////////////////////////////
     // Keygen Operation
-    ////////////////////////////////////////////////////////////
-    KeyPair<DCRTPoly> newKeyPair;
-    EvalKey<DCRTPoly> reKey;
+    KeyPair<DCRTPoly> newKeyPair = genKeyPair(cc);
+    EvalKey<DCRTPoly> reKey = genReKey(cc, keyPair1.secretKey, newKeyPair.publicKey);
 
-    newKeyPair = cc->KeyGen();
-    if (!newKeyPair.good()) {
-        std::cout << "New Key generation failed!" << std::endl;
-        return (false);
-    }
-    reKey = cc->ReKeyGen(keyPair1.secretKey, newKeyPair.publicKey);
-
-    ////////////////////////////////////////////////////////////
     // Re-Encryption
-    ////////////////////////////////////////////////////////////
     ct1 = cc->ReEncrypt(ct1, reKey);
 
-    ////////////////////////////////////////////////////////////
     // Decryption of Re-Encryption
-    ////////////////////////////////////////////////////////////
-    Plaintext ptDec2;
-    cc->Decrypt(newKeyPair.secretKey, ct1, &ptDec2);
-    ptDec2->SetLength(pt->GetLength());
+    Plaintext ptDec2 = decrypt(cc, newKeyPair.secretKey, ct1, ptSize);
 
     auto unpacked0 = pt->GetCoefPackedValue();
-    auto unpacked1 = ptDec1->GetCoefPackedValue();
-    auto unpacked2 = ptDec2->GetCoefPackedValue();
-
-    for(unsigned int j=0; j< pt->GetLength(); j++) {
-        if (unpacked1[j] < 0)
-            unpacked1[j] += plaintextModules;
-        if (unpacked2[j] < 0)
-            unpacked2[j] += plaintextModules;
-    }
+    auto unpacked1 = decodePlaintext(ptDec1, ptSize);
+    auto unpacked2 = decodePlaintext(ptDec2, ptSize);
 
     bool good = true;
     for (unsigned int j = 0; j < pt->GetLength(); j++) {
